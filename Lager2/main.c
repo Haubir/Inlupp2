c582a3023a4bfe3e35361cd614a0975d2fee0dfb
Haubir <jagheterhaubir@gmail.com>
2018-04-16 11:57:41 +0200

    Initial Commit

---
+++ Lager2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lager2
+//
+//  Created by Haubir Mariwani on 4/16/18.
+//  Copyright © 2018 HauCorp. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
