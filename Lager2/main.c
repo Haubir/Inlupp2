e906e811f2d75c62c78c7e1b2a3d06f3a67c0c8f
Haubir <jagheterhaubir@gmail.com>
2018-04-16 13:23:07 +0200

    Menu works

--- Lager2/main.c
+++ Lager2/main.c
@@ -6,11 +6,39 @@
 //  Copyright © 2018 HauCorp. All rights reserved.
 //
 
-#include <stdio.h>
+#include "main.h"
+
+char *menu_string() {
+  return "[L]ägg till en vara\n[T]a bort en vara\n[R]edigera en vara\nÅn[g]ra senaste ändringen\nLista [h]ela varukatalogen\n[P]acka en pall\n[A]vsluta\n\nVad vill du göra idag? ";
+}
 
 int main(int argc, const char * argv[]) {
   // insert code here...
   
-  printf("Hello, World!\n");
+  
+  while (1) {
+    char *input_string = calloc(1024, sizeof(char));
+    string_entry(menu_string(), input_string);
+    
+    char *input_string_tolower = calloc(1024, sizeof(char));
+    string_to_lower(input_string, input_string_tolower);
+    free(input_string);
+    
+    printf("input_string_tolower: %s\n", input_string_tolower);
+    strip_string(input_string_tolower);
+    
+    if (string_length(input_string_tolower) > 1){printf("Din input matchade inte något av alternativen från menyn. Försök igen.\n");}
+    else if (string_compare(input_string_tolower, "l") == 0) {printf("Du valde att lägga till en vara\n");}
+    else if (string_compare(input_string_tolower, "t") == 0) {printf("Du valde att ta bort en vara\n");}
+    else if (string_compare(input_string_tolower, "r") == 0) {printf("Du valde att redigera en vara\n");}
+    else if (string_compare(input_string_tolower, "g") == 0) {printf("Du valde att ångra den senaste ändringen\n");}
+    else if (string_compare(input_string_tolower, "h") == 0) {printf("Du valde att lista hela varukorgen\n");}
+    else if (string_compare(input_string_tolower, "p") == 0) {printf("Du valde att packa en pall\n");}
+    else if (string_compare(input_string_tolower, "a") == 0) {printf("Du valde att avsluta programmet\n"); exit(0);}
+    else {printf("Din input matchade inte något av alternativen från menyn. Försök igen.\n");}
+  }
+  
   return 0;
 }
+
+
---
+++ Lager2/main.h
@@ -0,0 +1,20 @@
+//
+//  main.h
+//  Lager2
+//
+//  Created by Haubir Mariwani on 4/16/18.
+//  Copyright © 2018 HauCorp. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+#include "string.h"
+
+/* Returns a string containing the menu of the program. */
+char *menu_string(void);
+
+/* Main function that starts, runs and shuts down the whole program. */
+int main(int argc, const char * argv[]);
+
+#endif /* main_h */
